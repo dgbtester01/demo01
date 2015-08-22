dc6352e095006c7717420ee3a1697c775cde63c8
Dave Burchill <david.burchill@nbcc.ca>
2015-08-22 08:47:53 -0400

    added Input, tested

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -3,11 +3,19 @@
 //
 
 #include <iostream>
+#include <string>
 
 int main()
 {
 	std::cout << "Game Over\n";
+	std::string name;
 
+	std::cout << "please enter your name:\n";
+
+	std::cin >> name;
+
+	std::cout << "Hello " << name << std::endl;
 	// debug cludge
+	getchar();
 	getchar();
 }
